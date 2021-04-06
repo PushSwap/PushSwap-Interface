6802f99f2f3f92ad9eb48e14e23105da74201bfb
malikfarhan786 <69869680+malikfarhan786@users.noreply.github.com>
2021-04-07 03:20:39 +0500

    Create lists.ts

---
+++ src/constants/lists.ts
@@ -0,0 +1,13 @@
+// used to mark unsupported tokens, these are hosted lists of unsupported tokens
+
+const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+
+export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
+
+// lower index == higher priority for token import
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  COMPOUND_LIST
+]
+
+// default lists to be 'active' aka searched across
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [GEMINI_LIST]
